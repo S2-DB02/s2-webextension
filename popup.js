7ce09ba676221473df86c13ab32a46045f9dd17f
Jeffrey Derksen <470568@student.fontys.nl>
2021-09-29 16:57:05 +0200

    Added basic login functionality.

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 // popup.js
 
-let userLoggedIn = true;
+let userEmail;
 
 function showPageElement(what)
 {
@@ -18,22 +18,53 @@ function hidePageElement(what)
     obj.style.display = "none";
 }
 
+function toggleLogin()
+{
+    if (userEmail != null) {
+        hidePageElement("login");
+        showPageElement("menu");
+        document.getElementById("loggedInUser").textContent = userEmail;
+    }
+    else {
+        hidePageElement("menu");
+        showPageElement("login");
+    }
+}
+
+function checkLogin()
+{
+    chrome.storage.sync.get("userEmail", (data) => {
+        if (data.userEmail != null){
+            userEmail = data.userEmail;
+        } else {
+            userEmail = null;
+        }
+        toggleLogin();
+    });
+}
+
+checkLogin();
+
 // Event listener for "Report bug" button
 document.getElementById("reportBugBtn").addEventListener("click", ()=>{
     chrome.windows.create({"url": "reportbug.html", "type": "popup"
     , "height": 600, "width": 600}, function(window) {})
-})
+});
 
 // Event listener for "Ticket overview" button
 document.getElementById("overviewBtn").addEventListener("click", ()=>{
     // Insert code to open new tab with ticket overview dashboard
-})
+});
 
-if (userLoggedIn) {
-    hidePageElement("login");
-    showPageElement("menu");
-}
-else {
-    hidePageElement("menu");
-    showPageElement("login");
-}
+// Event listener for "Log in" button
+document.getElementById("logInBtn").addEventListener("click", ()=>{
+    userEmail = document.getElementById("email").value;
+    chrome.storage.sync.set({ "userEmail": userEmail });
+    checkLogin();
+});
+
+// Event listener for "Log out" button
+document.getElementById("logOutBtn").addEventListener("click", ()=>{
+    chrome.storage.sync.remove("userEmail");
+    checkLogin();
+});
