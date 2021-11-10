ef43564b3e437d21f212f3b36ddeb7d46a8327c3
Faizal Susetyo <456826@student.fontys.nl>
2021-11-10 16:26:09 +0100

    Layed basic structure for login

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,18 @@
 // popup.js
+import {apiGetUserData} from '../modules/api_calls.js';
+import {apiGetAsJSON} from '../modules/api_calls.js';
+
+
+async function main() {
+  // Get form api url from config file
+  const configUrl = chrome.runtime.getURL('./config.json');
+  let apiUserUrl = await apiGetAsJSON(configUrl);
+  apiUserUrl = apiUserUrl['url_api_user'];
+  document.getElementById('login-form').action = apiUserUrl;
+}
+
+main();
+
 
 let userEmail;
 
