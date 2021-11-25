54120543cd7ff1dd685c051202ccac8e44a2cb81
SilverFS <f.susetyo@outlook.com>
2021-11-25 14:13:59 +0100

    Image wizardry of Anas

--- js/reportbug.js
+++ js/reportbug.js
@@ -1,6 +1,21 @@
 // reportbug.js
 import {apiGetAsJSON} from "../modules/api_calls.js";
 
+function dataURLtoFile(dataurl, filename) {
+ 
+  var arr = dataurl.split(','),
+      mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]), 
+      n = bstr.length, 
+      u8arr = new Uint8Array(n);
+
+  while(n--){
+      u8arr[n] = bstr.charCodeAt(n);
+  }
+
+  return new File([u8arr], filename, {type:mime});
+}
+
 async function main() {
 
   // Get form api url from config file
@@ -12,7 +27,21 @@ async function main() {
 
   chrome.storage.local.get("report_img", (imgdata) => {
     document.getElementsByClassName("screenshot")[0].src = imgdata.report_img;
-    document.getElementById("screenshotinput").value = imgdata.report_img;
+    // document.getElementsByName("photo")[0].value = imgdata.report_img;
+    var meh = imgdata.report_img;
+    console.log("meh");
+
+    var file =  dataURLtoFile(meh, "test.png");
+    console.log(meh);
+    document.getElementsByName("photo")[0].FileList = file;
+    // var image = new Image();
+    // image.src = imgdata.report_img;
+    // document.getElementsByName("photo")[0].filename = image;
+    // document.body.appendChild(image);
+
+    // var imageData = canvas.toDataURL('image/png');
+    // document.getElementsByName('photo')[0].setAttribute("value", imgdata.report_img)
+
   });
 
   // Get URL of the current active user window/tab
