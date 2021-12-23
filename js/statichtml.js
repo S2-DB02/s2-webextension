fb7857c2a834dc2b81de5954726f12994c4c94cc
SilverFS <f.susetyo@outlook.com>
2021-12-23 19:33:44 +0100

    General UI Improvements

---
+++ js/statichtml.js
@@ -0,0 +1,8 @@
+import '../js/jquery-3.5.1.min.js';
+
+function staticload() {
+    $('#static-header').load('../views/static-header.html');
+    $('#static-footer').load('../views/static-footer.html');
+};
+
+staticload();
