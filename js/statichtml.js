4b2319783ede0f9ccd1e7feabe541eeedc38dac6
SilverFS <f.susetyo@outlook.com>
2022-01-12 14:08:38 +0100

    Removed static-footer and added per page

--- js/statichtml.js
+++ js/statichtml.js
@@ -2,7 +2,6 @@ import '../js/jquery-3.5.1.min.js';
 
 function staticload() {
     $('#static-header').load('../views/static-header.html');
-    $('#static-footer').load('../views/static-footer.html');
 };
 
 staticload();
